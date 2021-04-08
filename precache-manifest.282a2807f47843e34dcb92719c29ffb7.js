87896f9df1d164c5480dcb7f2dd2ca891e569f22
FatemeSadeghi <f.sadeghii1998@gmail.com>
2021-04-09 01:34:01 +0430

    Updates

---
+++ precache-manifest.282a2807f47843e34dcb92719c29ffb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1af7d190acb9d52d28da",
+    "url": "/bnk48-face-recognition/static/css/main.a4e5f9f8.chunk.css"
+  },
+  {
+    "revision": "1af7d190acb9d52d28da",
+    "url": "/bnk48-face-recognition/static/js/main.1af7d190.chunk.js"
+  },
+  {
+    "revision": "59210e4715b077167f96",
+    "url": "/bnk48-face-recognition/static/js/1.59210e47.chunk.js"
+  },
+  {
+    "revision": "4942cd80093195c08172",
+    "url": "/bnk48-face-recognition/static/js/runtime~main.4942cd80.js"
+  },
+  {
+    "revision": "34e86f06102f347f0475d68f8673318b",
+    "url": "/bnk48-face-recognition/static/media/test.34e86f06.jpg"
+  },
+  {
+    "revision": "cc46e63d574915a8430eed2a3f4fe165",
+    "url": "/bnk48-face-recognition/static/media/example.cc46e63d.jpg"
+  },
+  {
+    "revision": "4b925ed03758d2887b1007d7929c31b6",
+    "url": "/bnk48-face-recognition/index.html"
+  }
+];
